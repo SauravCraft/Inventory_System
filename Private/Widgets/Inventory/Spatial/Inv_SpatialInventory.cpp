abcf32616a8e7ed8365f94302a0253de9c9eaa7e
SauravCraft <Sauravkumar7739s@gmail.com>
2026-05-13 02:06:51 +0530

    Widget Switcher Added Now We Can Switch b/w Widgets

--- Private/Widgets/Inventory/Spatial/Inv_SpatialInventory.cpp
+++ Private/Widgets/Inventory/Spatial/Inv_SpatialInventory.cpp
@@ -1,4 +1,44 @@
 
 
 #include "Widgets/Inventory/Spatial/Inv_SpatialInventory.h"
+#include "Widgets/Inventory/Spatial/Inv_InventoryGrid.h"
+#include "Components/Button.h"
+#include "Components/WidgetSwitcher.h"
 
+void UInv_SpatialInventory::NativeOnInitialized()
+{
+	EquippableButton->OnClicked.AddDynamic(this, &UInv_SpatialInventory::ShowEquippableGrid);
+	ConsumableButton->OnClicked.AddDynamic(this, &UInv_SpatialInventory::ShowConsumableGrid);
+	CraftableButton->OnClicked.AddDynamic(this, &UInv_SpatialInventory::ShowCraftableGrid);
+
+	ShowConsumableGrid();
+}
+
+void UInv_SpatialInventory::ShowEquippableGrid()
+{
+	SetActiveGrid(EquippableGrid, EquippableButton);
+}
+
+void UInv_SpatialInventory::ShowConsumableGrid()
+{
+	SetActiveGrid(ConsumableGrid, ConsumableButton);
+}
+
+void UInv_SpatialInventory::ShowCraftableGrid()
+{
+	SetActiveGrid(CraftableGrid, CraftableButton);
+}
+
+void UInv_SpatialInventory::DisableButton(UButton* Button)
+{
+	EquippableButton->SetIsEnabled(true);
+	ConsumableButton->SetIsEnabled(true);
+	CraftableButton->SetIsEnabled(true);
+	Button->SetIsEnabled(false);
+}
+
+void UInv_SpatialInventory::SetActiveGrid(UInv_InventoryGrid* Grid, UButton* Button)
+{
+	DisableButton(Button);
+	InventorySwitcher->SetActiveWidget(Grid);
+}
--- Public/Widgets/Inventory/Spatial/Inv_SpatialInventory.h
+++ Public/Widgets/Inventory/Spatial/Inv_SpatialInventory.h
@@ -5,9 +5,51 @@
 #include "Widgets/Inventory/InventoryBase/Inv_InventoryBase.h"
 #include "Inv_SpatialInventory.generated.h"
 
+
+class UInv_InventoryGrid;
+class UWidgetSwitcher;
+class UButton;
+
 UCLASS()
 class INVENTORYSYSTEM_API UInv_SpatialInventory : public UInv_InventoryBase
 {
 	GENERATED_BODY()
 	
+public:
+	virtual void NativeOnInitialized() override;
+
+private:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UWidgetSwitcher> InventorySwitcher;
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UInv_InventoryGrid> EquippableGrid;
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UInv_InventoryGrid> ConsumableGrid;
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UInv_InventoryGrid> CraftableGrid;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> EquippableButton;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> ConsumableButton;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> CraftableButton;
+
+
+	UFUNCTION()
+	void ShowEquippableGrid();
+
+	UFUNCTION()
+	void ShowConsumableGrid();
+
+	UFUNCTION()
+	void ShowCraftableGrid();
+
+
+	void DisableButton(UButton* Button);
+	void SetActiveGrid(UInv_InventoryGrid* Grid , UButton* Button);
+
+
 };
