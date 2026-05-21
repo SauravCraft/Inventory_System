cd6237974dc6a8125ec7f19449bbdfa1c24e79f0
SauravCraft <Sauravkumar7739s@gmail.com>
2026-05-21 08:31:26 +0530

    Fast Array Serilization Added

---
+++ Public/InventoryManagement/FastArray/Inv_FastArray.cpp
@@ -0,0 +1,40 @@
+#include "InventoryManagement/FastArray/Inv_FastArray.h"
+#include "Items/Inv_InventoryItem.h"
+
+
+void FInv_InventoryFastArray::PostReplicatedAdd(const TArray<int32> AddedIndex, int32 FinalSize)
+{
+
+
+}
+
+void FInv_InventoryFastArray::preReplicatedRemove(const TArray<int32> RemovedIndex, int32 FinalSize)
+{
+
+
+}
+
+TArray<FInv_InventoryFastArray> FInv_InventoryFastArray::GetAllEntries() const
+{
+	
+
+}
+
+UInv_InventoryItem FInv_InventoryFastArray::AddItem(UInv_InventoryItem* Item)
+{
+
+	
+}
+
+UInv_InventoryItem FInv_InventoryFastArray::AddItem(UInv_InventoryComponent* ItemComp)
+{
+	
+
+
+}
+
+void FInv_InventoryFastArray::RemoveItem(UInv_InventoryItem* Item)
+{
+
+
+}
---
+++ Public/InventoryManagement/FastArray/Inv_FastArray.h
@@ -0,0 +1,76 @@
+#pragma once
+#include "CoreMinimal.h"
+#include "Net/Serialization/FastArraySerializer.h"
+
+
+
+#include "Inv_FastArray.generated.h"
+class UInv_InventoryItem;
+class UInv_InventoryComponent;
+
+USTRUCT(BlueprintType)
+struct FInv_InventoryEntries : public FFastArraySerializerItem
+{
+	
+	GENERATED_BODY()
+
+	FInv_InventoryEntries();
+
+
+private:
+	friend struct FInv_InventoryFastArray;
+	friend UInv_InventoryComponent;
+
+	UPROPERTY()
+	TObjectPtr<UInv_InventoryItem> EntryItem = nullptr;
+
+
+};
+
+USTRUCT(BlueprintType)
+struct FInv_InventoryFastArray : public FFastArraySerializer
+{
+
+
+	GENERATED_BODY()
+
+	FInv_InventoryFastArray() : OwnerComponent(nullptr) {}
+	FInv_InventoryFastArray(UActorComponent* InOwnerComponent) : OwnerComponent(InOwnerComponent) {}
+
+	void PostReplicatedAdd( const TArray<int32> AddedIndex , int32 FinalSize);
+	void preReplicatedRemove(const TArray<int32> RemovedIndex , int32 FinalSize);
+
+
+
+
+	TArray<FInv_InventoryFastArray> GetAllEntries() const;
+
+	UInv_InventoryItem AddItem(UInv_InventoryItem* Item);
+	UInv_InventoryItem AddItem(UInv_InventoryComponent* ItemComp);
+
+	void RemoveItem(UInv_InventoryItem* Item);
+
+	bool NetDeltaSerilize(FNetDeltaSerializeInfo& DeltaParams)
+	{
+		return FastArrayDeltaSerialize<FInv_InventoryEntries, FInv_InventoryFastArray>(Entries, DeltaParams, *this);
+	}
+
+private:
+
+	friend UInv_InventoryComponent;
+
+	UPROPERTY(NotReplicated)
+	TObjectPtr<UActorComponent> OwnerComponent;
+
+	UPROPERTY()
+	TArray<FInv_InventoryEntries> Entries;
+};
+
+template<>
+struct TStructOpsTypeTraits<FInv_InventoryFastArray> : public TStructOpsTypeTraitsBase2<FInv_InventoryFastArray>
+{
+	enum
+	{
+		WithNetDeltaSerializer = true,
+	};
+};
